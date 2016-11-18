4e50700a651d982e2446189bb174cecdf5ede240
Shabeer Hussain <shabeershussain@gmail.com>
2016-11-18 17:36:48 +0000

    Initial Commit

---
+++ UIViewController+PopPresentable.swift
@@ -0,0 +1,24 @@
+//
+//  UIViewController+PopPresentable.swift
+//  JUSTEAT
+//
+//  Created by Shabeer Hussain on 17/10/2016.
+//  Copyright © 2016 Desert Monkey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIViewController {
+    
+    func presentViewControllerAsPopover(_ viewControllerToPresent: UIViewController, permittedArrowDirections: UIPopoverArrowDirection, sourceView: UIView?, sourceRect: CGRect, delegate:UIPopoverPresentationControllerDelegate?, animated: Bool, completion: (() -> Void)?) {
+        
+        viewControllerToPresent.modalPresentationStyle = .popover
+        viewControllerToPresent.popoverPresentationController?.sourceView = sourceView
+        viewControllerToPresent.popoverPresentationController?.sourceRect = sourceRect
+        viewControllerToPresent.popoverPresentationController?.delegate = delegate
+        viewControllerToPresent.popoverPresentationController?.permittedArrowDirections = permittedArrowDirections
+        
+        self.present(viewControllerToPresent, animated: animated, completion: completion)
+    }
+}
